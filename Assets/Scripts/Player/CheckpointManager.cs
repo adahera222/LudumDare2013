dcfc79d6af527406c3e19cf05cafc206790a6b1b
sigfig <0x516F16@Gmail.com>
2013-12-16 05:02:01 -0600

    some actual game thing

---
+++ Assets/Scripts/Player/CheckpointManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckpointManager : MonoBehaviour {
+
+	public RopeSpawner activerope;
+
+	void Start () {
+		activerope = GameObject.Find("Spawner").GetComponent<RopeSpawner>();
+	}
+	
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Rope/RopeSpawner.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RopeSpawner : MonoBehaviour {
+
+	public GameObject rope;
+
+	void Start() {
+		SpawnRope();
+	}
+
+	void SpawnRope() {
+		rope = new GameObject();
+		CreateRope ropecomponent = rope.AddComponent<CreateRope>();
+		ropecomponent.position = transform.position;
+		ropecomponent.ropeLength = 20;
+		ropecomponent.segmentLength = 0.25f;
+		ropecomponent.ropeThickness = 0.1f;
+
+		RopeRenderer vis = rope.AddComponent<RopeRenderer>();
+		vis.RopeWidth = 0.1f;
+	}
+}
