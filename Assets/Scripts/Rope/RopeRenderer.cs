01c07ed8e8ee80cc799139ed1940e18cdf5eaa8e
sigfig <0x516F16@Gmail.com>
2013-12-15 19:12:34 -0600

    pink ropes

---
+++ Assets/Scripts/Rope/RopeRenderer.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RopeRenderer : MonoBehaviour {
+
+	public float RopeWidth;
+
+	List<Vector3> vert;
+	List<int> tris;
+	List<Vector2> uvs;
+
+	CreateRope roperef;
+	Mesh ropemesh;
+
+	void Start () {
+		roperef = GetComponent<CreateRope>();
+		vert = new List<Vector3>();
+		tris = new List<int>();
+		uvs = new List<Vector2>();
+
+		ropemesh = new Mesh();
+		GetComponent<MeshFilter>().mesh = ropemesh;
+	}
+
+	void Update () {
+
+
+		vert.Clear();
+		tris.Clear();
+		uvs.Clear();
+
+		for (int i = 0; i < roperef.ropePieces.Length; i++) {
+			GameObject obj = roperef.ropePieces[i]; //ISN'T C GREAT
+
+			int trioffset = vert.Count;
+
+			Vector3 startpoint = obj.transform.TransformPoint(0, 0, -(roperef.segmentLengthScale * 5));
+			Vector3 endpoint = obj.transform.TransformPoint(0, 0, (roperef.segmentLengthScale * 5));
+
+			vert.Add(startpoint + new Vector3(0f, RopeWidth / 2, 0f));
+			vert.Add(startpoint + new Vector3(0f, -(RopeWidth / 2), 0f));
+			vert.Add(endpoint + new Vector3(0f, RopeWidth / 2, 0f));
+			vert.Add(endpoint + new Vector3(0f, -(RopeWidth / 2), 0f));
+
+			//CONTINUE FROM LAST SEGMENT
+			if(i != 0) {
+				tris.Add(trioffset - 2);
+				tris.Add(trioffset - 1);
+				tris.Add(trioffset + 1);
+
+				tris.Add(trioffset + 1);
+				tris.Add(trioffset);
+				tris.Add(trioffset - 2);
+			}
+			
+			//THINGS IN THE MIDDLE
+			tris.Add(trioffset);
+			tris.Add(trioffset + 1);
+			tris.Add(trioffset + 2);
+
+			tris.Add(trioffset + 1);
+			tris.Add(trioffset + 3);
+			tris.Add(trioffset + 2);
+
+			uvs.Add(new Vector2(1,1));
+			uvs.Add(new Vector2(1,1));
+			uvs.Add(new Vector2(1,1));
+			uvs.Add(new Vector2(1,1));
+		}
+
+		ropemesh.triangles = tris.ToArray();
+		ropemesh.vertices = vert.ToArray();
+		ropemesh.uv = uvs.ToArray();
+
+		//attach to filter
+
+		//GetComponent<MeshRenderer>().Render(0);
+	}
+}
